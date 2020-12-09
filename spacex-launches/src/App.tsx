6193f3d9f2a2df8d92a6ce22afc8a3571a842fac
Edward Cant <edward.alexander.cant@gmail.com>
2020-12-09 17:24:42 +0000

    Switch from CSS to SCSS using sass and create the header logo to verify that it's loading the font assets.

--- spacex-launches/src/App.tsx
+++ spacex-launches/src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import spacexLogo from './assets/spacex-logo.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={spacexLogo} className="header-bar__logo" alt="logo" />
+        <span className="header-bar__label">LAUNCHES</span>
       </header>
     </div>
   );
